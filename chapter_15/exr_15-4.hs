24641847aaab9302097685ac0ee77a75f5122785
dreipfundflachs <zuehlkep@gmail.com>
2022-05-06 21:20:14 -0300

    refactor: changed chapter numbers from 'd' to '0d'

--- chapter_15/exr_15-4.hs
+++ chapter_15/exr_15-4.hs
@@ -2,3 +2,5 @@
 --  Exercise 15.4 - Programming in Haskell - Hutton  --
 ------------------------------------------------------
 
+fibs :: [Integer]
+fibs = 0 : 1 : map (uncurry (+)) (zip fibs (tail fibs))
--- chapter_15/exr_15-6.hs
+++ chapter_15/exr_15-6.hs
@@ -4,16 +4,16 @@
 
 -- Given x, returns a function which computes the next approximation given the
 -- current approximation to the square root of x.
-next :: Double -> (Double -> Double)
-next x = \a -> ((a + x/a) / 2)
+next :: Double -> Double -> Double
+next x a = (a + x/a) / 2
 
 -- Calculates an approximation to the square root of a floating-point number x
 -- using Newton's approximation within an error of 0.00001.
 sqroot :: Double -> Double
-sqroot x = (snd . head . (filter p)) (zip ls (tail ls))
+sqroot x = (snd . head . (filter p)) (zip as (tail as))
            where
              p (x, y) = (abs (x - y) <= 0.00001)
-             ls = iterate (next x) 1.0
+             as = iterate (next x) 1.0
 
 -- Sample tests:
 -- abs (sqroot 3 - 1.73205080757) < 0.00001
